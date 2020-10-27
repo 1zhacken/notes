883acb8e7389b8a446b61b04b845e14773d97d13
1zhacken <35167681+1zhacken@users.noreply.github.com>
2020-10-27 09:59:38 +0200

    started making frontend

--- static/js/client.js
+++ static/js/client.js
@@ -1,20 +1,42 @@
 $(document).ready(() => {
-    /*$.post(
-        'api/add_note',
-        {'title': 'first note!', 'text': 'hello there'},
-        (json) => {
-            console.log('creating note => ' + json.status);
-        }
-    )*/
+    //config consts
+    const _notes_per_load = 10;
 
+    //data vars
+    let notes = [];
+    let last_note = 0;
+
+    // ---------------------------------------------------------- //
+
+    //automaticaly load first 10 notes
     $.post(
         'api/get_notes', 
-        {'start': 0, 'count': 10},
+        {'start': last_note, 'count': _notes_per_load},
         (json) => {
+            last_note += _notes_per_load;
             console.log('getting notes => ' + json.status);
-            var notes = json.notes;
-            console.log(notes)
+            notes = json.notes;
+            console.log(notes);
         }
-    )
-})
 
+        //load data to #notes-list here
+    );
+    
+    // ----------------------------------------------------------- //
+
+    $('load-more').click(() => {
+        $.post(
+            'api/get_notes', 
+            {'start': last_note, 'count': _notes_per_load},
+            (json) => {
+                last_note += _notes_per_load;
+                console.log('getting notes => ' + json.status);
+                notes = json.notes;
+            }
+        )
+
+        //update #notes-list here
+    });
+
+   // ------------------------------------------------------------ //
+})
